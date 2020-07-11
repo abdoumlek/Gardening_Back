5039787a2de12055e2afe47814ff655a0d0332eb
Youssef ABDELMALEK <abdelmalekyoussef@hotmail.com>
2020-07-11 16:37:12 +0200

    updated product model

--- app/Product.php
+++ app/Product.php
@@ -6,4 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
+    protected $fillable = ['name', 'description', 'photo', 'quantity', 'selling_price', 'buying_price', 'discount', 'category_id'];
+    protected $attributes = [
+        'reference' => '',
+        'photo' => '',
+        'quantity' => 0,
+        'buying_price' => 0,
+        'discount' => 0,
+    ];
 }
