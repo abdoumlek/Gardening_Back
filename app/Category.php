c5f2345b277bee408f4c909a92e01491d1caf7f2
Youssef ABDELMALEK <abdelmalekyoussef@hotmail.com>
2020-07-11 17:32:28 +0200

    added timestamps to enable adding categories

--- app/Category.php
+++ app/Category.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    //only the name is mandatory for category
+    protected $fillable = ['name', 'description'];
+    protected $attributes = [
+        'description' => '',
+    ];
 }
--- app/Product.php
+++ app/Product.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
+    // name , description , selling price , and category_id are mandatory
+    // the rest is not
     protected $fillable = ['name', 'description', 'photo', 'quantity', 'selling_price', 'buying_price', 'discount', 'category_id'];
     protected $attributes = [
         'reference' => '',
